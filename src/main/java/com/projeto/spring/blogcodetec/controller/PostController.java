beb6b66b7f40b4b5d0d2068198a820a809433011
Leandro Amorim <leandroamorimpb@gmail.com>
2021-06-15 14:55:13 -0300

    Criando a página postDetails.html buscando o post pelo id

--- src/main/java/com/projeto/spring/blogcodetec/controller/PostController.java
+++ src/main/java/com/projeto/spring/blogcodetec/controller/PostController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,6 +18,7 @@ public class PostController {
 	@Autowired
 	private PostService postService;
 	
+	//lista todos os posts cadastrados no banco de dados
 	@RequestMapping(value = "/posts", method = RequestMethod.GET)
 	public ModelAndView getPosts() {
 		
@@ -24,6 +26,16 @@ public class PostController {
 		List<Post> posts = postService.findAll();
 		
 		mv.addObject("posts", posts);
+		return mv;
+	}
+	
+	@RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+	public ModelAndView getPostDetails(@PathVariable ("id") Long id) {
+		
+		ModelAndView mv = new ModelAndView("postDetails");
+		Post post = postService.findById(id);
+		
+		mv.addObject("post", post);
 		return mv;
 	}
 }
