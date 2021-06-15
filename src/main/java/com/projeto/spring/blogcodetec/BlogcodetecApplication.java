b7507b00af5c985edbf2c2aa9a251ef865394dbf
Leandro Amorim <leandroamorimpb@gmail.com>
2021-06-15 08:20:06 -0300

    Criando o projeto e configurando o acesso ao banco de dados postgresql

---
+++ src/main/java/com/projeto/spring/blogcodetec/BlogcodetecApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.spring.blogcodetec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogcodetecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogcodetecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/spring/blogcodetec/BlogcodetecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.spring.blogcodetec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogcodetecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
