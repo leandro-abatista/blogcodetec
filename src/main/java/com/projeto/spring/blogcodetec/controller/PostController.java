7277a1f3681aa0d03fe51c172b1037a2afddf181
Leandro Amorim <leandroamorimpb@gmail.com>
2021-06-16 08:05:51 -0300

    Criando o formulário de publicação de post

--- src/main/java/com/projeto/spring/blogcodetec/controller/PostController.java
+++ src/main/java/com/projeto/spring/blogcodetec/controller/PostController.java
@@ -1,13 +1,18 @@
 package com.projeto.spring.blogcodetec.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.projeto.spring.blogcodetec.models.Post;
 import com.projeto.spring.blogcodetec.service.PostService;
@@ -37,5 +42,22 @@ public class PostController {
 		
 		mv.addObject("post", post);
 		return mv;
+	}
+	
+	@RequestMapping(value = "/newpost", method = RequestMethod.GET)
+	public String getPostForm() {
+		return "postForm";
+	}
+	
+	@RequestMapping(value = "/newpost", method = RequestMethod.POST)
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()) {//se for encontrado algum tipo de erro
+			return "redirect:/newpost";//volta para a página do newpost
+		}
+		
+		post.setData(LocalDate.now());
+		postService.save(post);
+		
+		return "redirect:/posts";
 	}
 }
